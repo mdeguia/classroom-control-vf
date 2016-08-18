e6ddeb83d1fce5192b15c8f8f3392cca01bbac1e
mdeguia <deguiamiko@gmail.com>
2016-08-19 07:42:08 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,25 +2,25 @@ class nginx {
   
   case $::osfamily {
     'RedHat','Debian' : {
-      $package = 'nginx',
-      $owner = 'root',
-      $group = 'root',
-      $docroot = '/var/www',
-      $confdir = '/etc/nginx',
-      $logdir = '/var/log/nginx',
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
     }
  
     'Windows' : {
-      $package = 'nginx-service',
-      $owner = 'Administrator',
-      $group = 'Administrators',
-      $docroot = 'C:/ProgramData/nginx/html',
-      $confdir = 'C:/ProgramData/nginx',
-      $logdir = 'C:/ProgramData/nginx/logs',
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
     }
     
  default : {
-    notify { "This ${::osfamily} is not supported. Rebuild your machine.": },
+    notify { "This ${::osfamily} is not supported": },
     }
     
   }
