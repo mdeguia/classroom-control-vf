249a5fd6ab53374b618b8c5b6a4e5cd58b313b24
mdeguia <deguiamiko@gmail.com>
2016-08-18 12:11:37 +0800

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,5 +1,5 @@
-class users:admins {
-  users:managed_user {'jose':}
-  users:managed_user {'alice':}
-  users:managed_user {'chen':}
+class users::admins {
+  users::managed_user {'jose':}
+  users::managed_user {'alice':}
+  users::managed_user {'chen':}
 }
