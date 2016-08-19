9aba2e7ab0813bc39c50460411b4ae3e6a23ad15
mdeguia <deguiamiko@gmail.com>
2016-08-19 11:21:37 +0800

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,26 @@
+class nginx::params {
+  
+  case $::osfamily {
+    'RedHat','Debian' : {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
+ 
+    'Windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+    
+    default : {
+    notify { "This is not supported": }
+    }
+  }
+}
